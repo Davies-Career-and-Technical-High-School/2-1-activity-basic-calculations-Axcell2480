1eb6330277d96893934e27a450b1421c516087ab
Axcell Collins-Mena <axcell.collins-mena@daviestech.org>
2020-10-08 22:54:50 +0000

    Order of operations and used float feature to add decimals

---
+++ question2.py
@@ -0,0 +1,12 @@
+f = float(input("Enter a Decimal number: "))
+x = float(input("Enter a Decimal number: "))
+y = float(input("Enter a Decimal number: "))
+
+print("Sum is: " + str(f + x + y))
+
+
+
+
+print( 16 - 2 * 5 // 3 + 1)
+
+print(2 ** 2 ** 3 * 3)
